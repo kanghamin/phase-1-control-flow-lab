92972856bbdbbe2efcdd85d7fdc6bc5caeab6636
kanghamin <gogokhm1@gmail.com>
2021-12-13 16:18:04 -0700

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(number){
+  if (number <= 400) {
+    return 'This one is on me!'
+  } else if (number > 2000) {
+    if (number > 2500) {
+      return "No can do."
+    } else {
+      return "I will gladly take your thirty bucks."
+    }
+  } 
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  if (city === 'NYC') {
+    return 'Ok, sounds good.'
+  } else {
+    return 'No go.'
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(attitude){
+  if (attitude === 'generous') {
+    return 'Thank you so much.'
+  } else if (attitude === 'not as generous') {
+    return 'Thank you.'
+  } else {
+    return 'Bye.'
+  }
 }
